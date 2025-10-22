bfcd75faa0195d2d142ebffa76d6bb07997d7eae
Leon König <koenig.leon2000@gmail.com>
2025-10-22 11:02:35 +0200

    initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HeroPageComponent } from './hero-page/hero-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HeroPageComponent}
+];
---
+++ src/app/generate-recipe/generate-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './generate-recipe.component.html',
+  styleUrl: './generate-recipe.component.scss'
+})
+export class GenerateRecipeComponent {
+
+}
