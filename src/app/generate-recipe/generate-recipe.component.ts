df51533b306e78dc85e6aa2f57a0c5b34f37bbb2
Leon König <koenig.leon2000@gmail.com>
2025-10-23 18:05:21 +0200

    ingredient list component

--- src/app/generate-recipe/generate-recipe.component.ts
+++ src/app/generate-recipe/generate-recipe.component.ts
@@ -1,12 +1,29 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { SingleIngredientComponent } from './single-ingredient/single-ingredient.component';
 
 @Component({
   selector: 'app-generate-recipe',
   standalone: true,
-  imports: [],
+  imports: [ NgIf, NgFor, SingleIngredientComponent ],
   templateUrl: './generate-recipe.component.html',
   styleUrl: './generate-recipe.component.scss'
 })
 export class GenerateRecipeComponent {
+  isOpen = false;
+  selectedUnit = 'gram';
+  units = ['piece', 'ml', 'gram'];
 
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectUnit(unit: string) {
+    this.selectedUnit = unit;
+    this.isOpen = false; // close dropdown after selection
+  }
+
+  ingredients = [
+    { name: 'Tomato', quantity: 2, unit: 'piece' },
+  ];
 }
---
+++ src/app/generate-recipe/single-ingredient/single-ingredient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-ingredient',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-ingredient.component.html',
+  styleUrl: './single-ingredient.component.scss'
+})
+export class SingleIngredientComponent {
+
+}
