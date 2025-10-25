e6df4ef6134231c32b4f97e5ba1b80eacf1e152e
Leon König <koenig.leon2000@gmail.com>
2025-10-25 14:25:36 +0200

    cleanup

--- src/app/generate-recipe/generate-recipe.component.ts
+++ src/app/generate-recipe/generate-recipe.component.ts
@@ -7,26 +7,10 @@ import { ServingComponent } from './serving/serving.component';
 @Component({
   selector: 'app-generate-recipe',
   standalone: true,
-  imports: [ NgIf, NgFor, SingleIngredientComponent, RouterLink, ServingComponent],
+  imports: [ NgFor, SingleIngredientComponent, RouterLink, ServingComponent],
   templateUrl: './generate-recipe.component.html',
   styleUrl: './generate-recipe.component.scss'
 })
 export class GenerateRecipeComponent {
-  isOpen = false;
-  selectedUnit = 'gram';
-  units = ['piece', 'ml', 'gram'];
-
-  toggleDropdown() {
-    this.isOpen = !this.isOpen;
-  }
-
-  selectUnit(unit: string) {
-    this.selectedUnit = unit;
-    this.isOpen = false; // close dropdown after selection
-  }
-
-  ingredients = [
-    { name: 'Tomato', quantity: 2, unit: 'piece' },
-    { name: 'Tomato', quantity: 2, unit: 'piece' },
-  ];
+  ingredients = [];
 }
--- src/app/generate-recipe/single-ingredient/single-ingredient.component.ts
+++ src/app/generate-recipe/single-ingredient/single-ingredient.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
 import { ServingComponent } from '../serving/serving.component';
-import { NgIf, NgClass } from '@angular/common';
-import { animate, style, transition, trigger } from '@angular/animations';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-single-ingredient',
   standalone: true,
-  imports: [NgIf, ServingComponent, NgClass],
+  imports: [NgIf, ServingComponent],
   templateUrl: './single-ingredient.component.html',
   styleUrl: './single-ingredient.component.scss',
-  animations: [
-    trigger('fadeInOut', [
-      transition(':enter', [  // when element appears
-        style({ opacity: 0 }),
-        animate('300ms ease-in', style({ opacity: 1 }))
-      ]),
-      transition(':leave', [  // when element disappears
-        animate('300ms ease-out', style({ opacity: 0 }))
-      ])
-    ])
-  ]
 })
 export class SingleIngredientComponent {
   editMode_enabled: boolean = false;
