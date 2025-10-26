a3c8fa34f575a67c6cd0b55c1e96c68a8a11e7f9
Beslim17 <b.montiari@gmail.com>
2025-10-26 22:39:51 +0100

    add full heart to recipe page

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
 import { InstructionsComponent } from './instructions/instructions.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [HeaderComponent, IngredientsComponent, InstructionsComponent],
+  imports: [HeaderComponent, IngredientsComponent, InstructionsComponent, RouterLink],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
