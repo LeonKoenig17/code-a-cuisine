b81ea81650e7075e4ca9a8d6e52796d04887256b
Leon König <koenig.leon2000@gmail.com>
2025-11-21 11:01:45 +0100

    hero page responsive complete

--- src/app/hero-page/hero-page.component.ts
+++ src/app/hero-page/hero-page.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-hero-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './hero-page.component.html',
   styleUrl: './hero-page.component.scss'
 })
 export class HeroPageComponent {
+  isMobile = window.innerWidth < 800;
 
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth < 800;
+  }
 }
