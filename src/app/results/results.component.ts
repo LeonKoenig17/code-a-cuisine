2d68c69d6dc6756fd8205ffbd53e082b8bbe27cb
Leon König <koenig.leon2000@gmail.com>
2025-11-27 11:50:49 +0100

    results page responsive complete

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SingleResultComponent } from './single-result/single-result.component';
 import { DataService } from '../shared/services/data.service';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [SingleResultComponent],
+  imports: [SingleResultComponent, NgIf],
   templateUrl: './results.component.html',
   styleUrl: './results.component.scss'
 })
 export class ResultsComponent {
   constructor(private dataService: DataService) {}
+
+  isMobile = window.innerWidth < 800;
+      
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth < 800;
+  }
 
   ngOnInit() {
     this.dataService.loadData();
